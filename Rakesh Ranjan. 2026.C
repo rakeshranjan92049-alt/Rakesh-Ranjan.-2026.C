441f635761ff251192582b29c7d1f258aaf7e0ce
Rakesh Ranjan <rakeshranjan92049@gmail.com>
2026-03-11 18:55:39 +0530

    Create Rakesh Ranjan. 2026.C

---
+++ Rakesh Ranjan. 2026.C
@@ -0,0 +1,39 @@
+HISTORY of WEB Development 
+        Duniya ka pehla website bani tha 
+        6 Aug 1991 me;
+         WEBSITE naam=info.cern.ch;
+         Aadmi ka naam =TIM BERNERS lee ;\British tha;
+         CoUntry=UK;
+         CAPITAL =LONDON ;
+         Baad me unhone Switzerland ke CERN me kaam karte hue World Wide Web ka invention kiya.
+         OBSERVE =jab wo cern me kaam  kar rahe the
+         tab unhone jorurat felt kiya ki  hume
+         data share karna hota hai ,infoshare karna hota hai
+         \across the reseach organisation ;
+         TIM BERNERS LEE purpose =data share ko simple karna;
+  
+  F2
+  Haan 👍 tumhara point mostly sahi direction me hai, bas thoda sa refine kar dete hain taaki exam me 100% correct lage.
+Pehle kya hota tha?
+Internet par computers connected hote the, lekin information access karna easy nahi tha. Logon ko:
+Direct connection banana padta tha
+Specific computer ka address pata hona chahiye tha
+Mostly scientists aur researchers hi use karte the
+Phir aaye 👉 Tim Berners-Lee
+Unhone 1989 me CERN me kaam karte waqt ek system banaya jise hum aaj World Wide Web ke naam se jaante hain.
+Unka main purpose kya tha?
+✔ Information ko easily share karna
+✔ Different computers par stored data ko link karna
+✔ Kisi bhi internet se connected insaan ko information access karne dena
+Unhone 3 main cheezein di:
+HTML (web page banane ke liye)
+HTTP (data transfer ke liye)
+URL system (address system)
+🔹 Isliye tumhara statement better tarike se aise likha ja sakta hai:
+Pehle information sirf limited connected computers tak hi pahuchti thi, lekin Tim Berners-Lee ne World Wide Web banaya taki internet se jude har vyakti tak website ke madhyam se information pauche sake;
+HTML, HTTP/HTTPS aur URL, ye sab World Wide Web ke components hain jo website ko banane, access karne aur data transfer karne me madad karte hain.
+Ek simple example
+🏗 Web Development = Ghar banana
+🏠 Website = Ghar
+🌆 WWW = Shehar (jahan bahut saare ghar hain)
+Web development me mainly HTML, CSS aur JavaScript ka use hota hai,;
